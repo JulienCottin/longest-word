67769fa970b62f765e5e76b16669af13125e38dd
Julien COTTIN <yandel77@wanadoo.fr>
2020-10-06 16:18:40 +0200

    game

--- game.py
+++ game.py
@@ -7,3 +7,11 @@ class Game :
         self.grid=[]
         for _ in range(9) :
             self.grid.append(random.choice(string.ascii_uppercase))
+
+    def is_valid(self,user_word) :
+        if user_word == '' :
+            return False
+        for letter in user_word :
+            if letter not in self.grid :
+                return False
+        return True
